f9a6a312741c23cde6ea7615307ef70c1e9fec3a
nuraalhaji <danamonuraa@gmail.com>
2025-02-18 13:48:44 +0100

    commit

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const multer = require("multer")
 const port = 3000
 
 const app = express()
+const storage = multer.diskStorage({ destination : "uploads/",
+    filename:(req, file, cb)=> {
+cb(null,`${file.originalname}`)
+}})
+
+app.use("/file", express.static("uploads/"))
+const upload = multer({storage:storage})
+
 
 app.use(cors())
 app.get("/",(req,res)=>{
   res.send("hello from backend by DNA")
+})
+app.post("/post",upload.single("file"),(req,res)=>{
+    console.log(req.file)
 })
 
 app.listen(port,()=>{
